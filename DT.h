a0b1abb9be0fa98c4b4c5e97f3a44670386a0ee0
WinterMishka <130703889+WinterMishka@users.noreply.github.com>
2023-06-14 21:32:54 +0300

    Update DT.h

--- DT.h
+++ DT.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <cstdlib>
-#include <iostream>
+#include "GasStation.h"
+#include "DT.h"
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib")
 
 namespace WinterMishka {
 
@@ -9,53 +11,48 @@ namespace WinterMishka {
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
+	using namespace System::Data::OleDb;
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Сводка для DT
+	/// Сводка для Start
 	/// </summary>
-	public ref class DT : public System::Windows::Forms::Form
+	public ref class Start : public System::Windows::Forms::Form
 	{
 	public:
-	private:
-		String^ _price_DT;
-	public:
-		property String^ Value1 {
-			void set(String^ price_DT) { _price_DT = price_DT; }
-			String^ get() { return _price_DT; }
-		}
-		DT(void)
+		Start(void)
 		{
 			InitializeComponent();
 			//
 			//TODO: добавьте код конструктора
 			//
-			DT::StartPosition = FormStartPosition::CenterScreen;
+			Start::StartPosition = FormStartPosition::CenterScreen;
 		}
 
 	protected:
 		/// <summary>
 		/// Освободить все используемые ресурсы.
 		/// </summary>
-		~DT()
+		~Start()
 		{
 			if (components)
 			{
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+
+	private: System::ComponentModel::IContainer^ components;
+
 	protected:
 
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -64,103 +61,97 @@ namespace WinterMishka {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// label1
+			// button1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Monotype Corsiva", 13.875F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+			this->button1->Location = System::Drawing::Point(1496, 23);
+			this->button1->Margin = System::Windows::Forms::Padding(6);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(80, 77);
+			this->button1->TabIndex = 0;
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Start::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Monotype Corsiva", 36, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(415, 62);
-			this->label1->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(714, 45);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Добро пожаловать на заправочную колонку №";
-			this->label1->Click += gcnew System::EventHandler(this, &DT::label1_Click);
+			this->button2->Location = System::Drawing::Point(306, 479);
+			this->button2->Margin = System::Windows::Forms::Padding(6);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(1000, 154);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Заправиться!";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Start::button2_Click);
 			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(473, 332);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(185, 31);
-			this->textBox1->TabIndex = 1;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &DT::textBox1_TextChanged);
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Monotype Corsiva", 19.875F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
-				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(73, 178);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(622, 64);
-			this->label2->TabIndex = 2;
-			this->label2->Text = L"Введите количество литров:";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Monotype Corsiva", 13.875F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
-				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(203, 254);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(395, 45);
-			this->label3->TabIndex = 3;
-			this->label3->Text = L"Цена 1 литра топлива = ";
-			this->label3->Click += gcnew System::EventHandler(this, &DT::label3_Click);
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(216, 421);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(70, 25);
-			this->label4->TabIndex = 4;
-			this->label4->Text = L"label4";
-			this->label4->Click += gcnew System::EventHandler(this, &DT::label4_Click);
-			// 
-			// DT
+			// Start
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1600, 1154);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label1);
+			this->ClientSize = System::Drawing::Size(1600, 1062);
+			this->ControlBox = false;
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Margin = System::Windows::Forms::Padding(6);
-			this->Name = L"DT";
-			this->Text = L"DT";
-			this->Load += gcnew System::EventHandler(this, &DT::DT_Load);
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"Start";
+			this->Text = L"Start";
+			this->Load += gcnew System::EventHandler(this, &Start::Start_Load);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
-		int litr;
-	private: System::Void DT_Load(System::Object^ sender, System::EventArgs^ e) {
-		int a = rand() % 5 + 1;
-		label3->Text = "Цена за 1 литр топлива = " + _price_DT;
-		label1->Text = System::String::Concat("Добро пожаловать на заправочную колонку №" + a);
+	String^ value;
+	String^ price_AI92;
+	String^ price_AI95;
+	String^ price_AI98;
+	public:
+		static String^ price_DT;
+	private: System::Void Start_Load(System::Object^ sender, System::EventArgs^ e) {
+		if (InternetCheckConnection(L"http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0)) {
+			MessageBox::Show("Интернет успешно подключен!");
+			String^ connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\artyo\\source\\repos\\WinterMishka\\BD.accdb;";
+			OleDbConnection^ connection = gcnew OleDbConnection(connectionString);
+			OleDbDataAdapter^ adapter = gcnew OleDbDataAdapter("SELECT * FROM [password]", connection);
+			if (connection->State == ConnectionState::Closed) {
+				MessageBox::Show("База данных успешно подключена!");
+				connection->Open();
+				OleDbDataAdapter^ adapter = gcnew OleDbDataAdapter("SELECT * FROM [password]", connection);
+				OleDbDataAdapter^ adapter2 = gcnew OleDbDataAdapter("SELECT * FROM [datasource]", connection);
+				DataTable^ dataTable = gcnew DataTable();
+				DataTable^ dataTable2 = gcnew DataTable();
+				adapter->Fill(dataTable);
+				adapter2->Fill(dataTable2);
+				value = dataTable->Rows[0]->default[1]->ToString();
+				price_AI92 = dataTable2->Rows[0]->default[2]->ToString();
+				price_AI95 = dataTable2->Rows[1]->default[2]->ToString();
+				price_AI98 = dataTable2->Rows[2]->default[2]->ToString();
+				price_DT = dataTable2->Rows[3]->default[2]->ToString();
+				connection->Close();
+			}
+			else {
+				MessageBox::Show("Не удалось установить соединение с базой данных.");
+			}
+		}
+		else {
+			MessageBox::Show("Отсутствует подключение к интернету!");
+			Application::Exit();
+		}
 	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		Application::Exit();
 	}
-	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		GasStation^ f1 = gcnew GasStation();
+		f1->Value = value;
+		f1->Owner = this;
+		f1->Show();
+		this->Hide();
 	}
-	private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-	//litr = Convert::ToInt32(textBox1->Text);
-	//float sum = litr * priceDT;
-	//label4->Text = ("Итого к оплате = " + (sum))->ToString();
-	//label4->Refresh();
-}
 };
 }
